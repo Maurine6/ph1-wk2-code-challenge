addfaa796945b20d76e8be895725be3e2142087a
omondimaurine <omondimaurine713@gmail.com>
2024-03-30 15:01:28 +0300

    using two numbers to generate an array

--- twoNumbers.js
+++ twoNumbers.js
@@ -1,22 +1,22 @@
 
 //write a function that uses two numbers to generate an array. 
-// in this case, pass two parameters in our function which indicates starting number and end.
+// in this case, we pass two parameters in our function which indicates starting number and end.
 function generateArray(startingNumber, end){
     
     // initialize our array, which empty still.
     let y = [];
     
-// use a for loop, to get the numbers as below.
+// use a for loop, to get the numbers in between as below.
     for ( i = startingNumber; i <= end; i++){
-        // after getting the number, we now push it to our array.
-        // now our array is supposed to have something.
-        y.push(i)
+        // after getting the numbers, we now push it to our array.
         
-
+        y.push(i);
+        
+//  now our array is supposed to have something.
     }
     //return the generated array.
     return y;
 };
 // test the function by calling it using different pair of numbers.
-// use console.log() check the output.
+// use console.log() to check the output.
 console.log(generateArray(4,7));
